cc7e570d9cb1a2231307ab53e8a6ca9874ea2c98
joaobru <joaopaulobruno123@gmail.com>
2020-08-25 16:18:51 -0300

    Last Commit

--- src/components/listJourneys/index.jsx
+++ src/components/listJourneys/index.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import './styles.css'
+import './styles.css';
 
 import bedIcon from '../../assets/icons/bed.svg';
 import checkIcon from '../../assets/icons/check.svg';
@@ -8,30 +8,36 @@ import paperPlaneIcon from '../../assets/icons/paper-plane.svg';
 import penIcon from '../../assets/icons/pen.svg';
 import playCircleIcon from '../../assets/icons/play-circle.svg';
 
-const objectIcon = [ paperPlaneIcon, playCircleIcon, penIcon, bedIcon, checkIcon]
+const objectIcon = [ paperPlaneIcon, playCircleIcon, penIcon, bedIcon, checkIcon];
+const objectJourneys = ['Enviando','Ativa', 'Configurando', 'Ociosa', 'Concluida'];
 
 function ListJourneys(props) { 
 
-  let count = 0;
+  let counter = 0;
 
   function iconsVerify(status) {
     
     for (let i = 1; i <= objectIcon.length; i++) {
       if (status === i) {
-        return <img src={objectIcon[count]}/>
-      }
-      count ++
+      return (
+      <>
+        <img src={objectIcon[counter]} alt={objectJourneys[counter]}/>
+        {objectJourneys[counter]}
+      </>
+      )
     }
+    counter ++;
   }
+}
 
   return(
     <div className= 'journeys'>
       <p id={props.idJourney} className='nameJourney'>{props.name}</p>
       <p id={props.idJourney} className='infoJourney'>{props.destinatary}</p>
       <p id={props.idJourney} className='infoJourney'>{props.success}</p>    
-      <p id={props.idJourney} className='infoJourney'>{iconsVerify(props.status)}</p> 
+      <p id={props.idJourney} className='statusJourney'>{iconsVerify(props.status)}</p> 
     </div>
   );
 }
 
-export default ListJourneys
+export default ListJourneys;
